b5806995e2d0297679fe16d8f701e113b3404af2
John Quiroga <quirjohn@colpatria.com>
2020-02-16 11:36:12 -0500

    first web app version

---
+++ precache-manifest.4b73ef4649bea55edaa2f36640d9b66d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14557e13b1879a57db61bbff0612b2ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cdaae4ec686a35701ac",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fed89c41ad9f81c1559e",
+    "url": "/static/js/2.3618c4e3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.3618c4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cdaae4ec686a35701ac",
+    "url": "/static/js/main.c619d8be.chunk.js"
+  },
+  {
+    "revision": "163794a7343a000e5e6a",
+    "url": "/static/js/runtime-main.7741245b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
